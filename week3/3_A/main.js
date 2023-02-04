cf5db7a8ba8e76da253ef1c17b3a986139f1a3eb
Camille <c.gonzalez0017@gmail.com>
2023-02-04 14:30:32 -0600

    commit

---
+++ week3/3_A/main.js
@@ -0,0 +1,26 @@
+let rotateBy = 5;
+function setup() {
+    createCanvas(600, 600);
+    background(0);
+    angleMode(DEGREES);
+}
+
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360);
+    noFill();
+    stroke(255);
+    strokeWeight(1);
+    /* ellipse(150, 150 + alt, 150 - alt); */
+    bezier(alt - 10, alt + 10, 10, 10, 160, 90, 50, 80)
+}
+
+function draw() {
+    translate(300, 300);
+    rotate(rotateBy); //come back
+    makeArm(rotateBy);
+    rotateBy += 5;
+}
+
+function mousePressed() {
+    noLoop();
+}
