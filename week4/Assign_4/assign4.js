803646d0c750cdc55d4ec17e8101825d4a7cf706
Camille <c.gonzalez0017@gmail.com>
2023-02-12 17:46:55 -0600

    commit fixed coding

--- week4/Assign_4/assign4.js
+++ week4/Assign_4/assign4.js
@@ -1,19 +1,37 @@
+let myCards = [];
 const rectWidth = 300;
 const rectHeight = 200;
 let startingX = 100;
 let startingY = 100;
-let myRect = [];
+
 function setup() {
-    createCanvas(1400, 1800);
-    background(0);
-/*     rect(rectX, rectY, rectWidth, rectHeight); */
-    for(let i = 0; i < 20; i++) {
-        rect(startingX, startingY, rectWidth, rectHeight);
-        startingX += 150;
+  createCanvas(1800, 1400);
+  background(0);
+  for (let k = 0; k < 4; k++) {
+    for (let i = 0; i < 4; i++) {
+      rect(startingX, startingY, rectWidth, rectHeight);
+      myCards.push({
+        x: startingX,
+        y: startingY,
+      });
+      startingX += 350;
     }
+    startingY += 350;
+    startingX = 100;
+  }
+  console.log(myCards);
 }
-function mousePressed() {
-    if(mouseX > rectX && mouseX < rectX + rectWidth && mouseY > rectY && mouseY < rectY + rectHeight) {
-        console.log("square has been hit");
+/* function mousePressed() {
+  console.log(myCards);
+  for (let k = 0; myCards.length; k++) {
+    if (
+      mouseX > myCards[k].x &&
+      mouseX < myCards[k].x + rectWidth &&
+      mouseY > myCards[k].y &&
+      mouseY < myCards[k].y + rectHeight
+    ) {
+      console.log("square has been hit");
     }
-}
+  }
+} */
+// command shift and p prettier and command d selects everything below
