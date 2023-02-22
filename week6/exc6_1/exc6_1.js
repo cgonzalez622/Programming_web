88de0cb593bfe8255516b2c112af5c98e3e99ae0
Camille <c.gonzalez0017@gmail.com>
2023-02-21 20:16:38 -0600

    commit exc6_1

---
+++ week6/exc6_1/exc6_1.js
@@ -0,0 +1,11 @@
+Vue.createApp({ //or const {createApp} = Vue;
+    data () {
+        return{
+            name: "ken", 
+            age: 52
+        }
+    }
+}).mount("#myVueApp");
+
+//what does this mean ... you will be making your own components that look like html
+//how is a interface put together -->start thinking about it 
