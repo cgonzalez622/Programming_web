83cfd0bb6a0cdce8d252dca730a4db291cdc1f18
Camille <c.gonzalez0017@gmail.com>
2023-02-28 16:26:10 -0600

    commit practice

---
+++ week7/practice/practice7.js
@@ -0,0 +1,24 @@
+const {createApp} = Vue; 
+createApp({
+    data () {
+        return {
+            currentRoute: "products"
+
+
+
+        /*  baseStyle: {
+            background: "red",
+            border: "1px solid black"
+        },
+        overRideStyle: {
+            background: "purple",
+            padding: "20px",
+            margin: "0 20px"
+        } */
+
+        /*happyLand: "https://www.youtube.com/",
+        happyTitle: "Happiest place on earth" */
+        }
+    }
+}).mount("#app");
+
