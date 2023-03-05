238596f9f42f0bd83c530977bde3fcb80f33f6c7
Camille <c.gonzalez0017@gmail.com>
2023-03-04 22:18:50 -0600

    commit

--- week7/assign7/assign7.js
+++ week7/assign7/assign7.js
@@ -3,7 +3,8 @@ createApp({
   data() {
     return {
             consoles: [{
-            image: "imgs/The School of Athens by Raphael.png",
+            artist: "The School of Athens by Raphael",
+            image: "imgs/The School of Athens by Raphael.jpg",
             movement: "Renaissance",
             time: "14th to 17th century",
             locations: "Italy, and gradually across Europe", 
@@ -11,6 +12,8 @@ createApp({
             description: "A shift from medieval period styles to the representational forms of mostly biblical scenes to include portraits, episodes from Classical religion, and culture."
             },
             {
+            artist: "Water Lilies Series by Claude Monet",
+            image: "imgs/Water Lilies Series by Claude Monet.jpg",
             movement: "Impressionism",
             time: "1867 and 1886",
             locations: "Key regions are France and England",
@@ -18,6 +21,8 @@ createApp({
             description: "A art movement that centered around the rebelling against classical subject matter and embraced modernity, desiring to create works of reality."
             },
             {
+            artist: "Bauhaus Exhibition Poster (1923)",
+            image: "imgs/Bauhaus Exhibition Poster (1923).jpg",
             movement: "Modernism",
             time: "late 19th and early 20th centuries",
             locations: "Global phenomenon",
@@ -25,6 +30,8 @@ createApp({
             description: "A movement that sought for the creation of new forms of art, philosophy, and social organization which reflected the newly emerging industrial world, including features such as urbanization, architecture, new technologies, and war."
             },
             {
+            artist: "Red Circle on Black, 1965 by Jiro Yoshihara",
+            image: "imgs/Red Circle on Black, 1965 by Jiro Yoshihara_.jpg",
             movement: "Minimalism",
             time: "early 1960s and present",
             locations: "Emerged in New York and would spread globally",
@@ -32,6 +39,8 @@ createApp({
             description: "A movement that exposed the essence, essentials or identity of a subject through eliminating all non-essential concepts."
             },
             {
+            artist: "Colour Tunnel by Olafur Eliasson",
+            image: "imgs/One-Way Colour Tunnel by Olafur Eliasson (2007).jpg",
             movement: "Contemporay",
             time: "20th century or in the 21st century",
             locations: "Global",
