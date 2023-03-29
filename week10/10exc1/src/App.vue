00937740715bd9fe70bb9becf3df978b25eba86a
Camille <c.gonzalez0017@gmail.com>
2023-03-28 22:47:32 -0500

    commit 10exc1

--- week10/10exc1/src/App.vue
+++ week10/10exc1/src/App.vue
@@ -1,85 +1,60 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/alpaca">Alpaca</RouterLink> |
+        <RouterLink to="/llama">Llama</RouterLink>
+    </nav>
+    <div class="container">
+        <RouterView></RouterView>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+html {
+    font-size: 16px;
+    font-family: Arial, Helvetica, sans-serif;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body, #app {
+    margin: 0;
 }
-
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+    width: 100%;
+    height: 45px;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+nav a, nav a:hover, nav a:link, nav a:active, nav a:visited {
+    color: antiquewhite;
+    font-weight: 900;
+    font-size: 20px;
+    line-height: 5;
+    text-decoration: none;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+nav a.router-link-active {
+    text-decoration: underline;
+    color: orange;
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.container {
+    max-width: 1200px; 
+    width: 100%; 
+    margin: 0 auto;
 }
-
-nav a:first-of-type {
-  border: 0;
+.container h1 {
+    margin-top: 150px;
+    text-align: center;
+    font-size: 30px;
 }
+.container p {
+    width: 1000px;
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+}
+.container img {
+    display: block;
+    max-width: 1000px;
+    width: 100%;
+    margin: 20px auto 20px;
 }
 </style>
--- week10/10exc1/src/router/index.js
+++ week10/10exc1/src/router/index.js
@@ -1,22 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router"
+import Home from "../views/Home.vue"
+import Alpaca from "../views/Alpaca.vue"
+import Llama from "../views/Llama.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: "/alpaca",
+      name: "alpaca",
+      component: Alpaca
+    },
+    {
+        path: "/llama",
+        name: "llama",
+        component: Llama
+      }
   ]
 })
 
