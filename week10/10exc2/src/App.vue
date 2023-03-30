b466b4aa288cd01f548483130df14006e7008543
Camille <c.gonzalez0017@gmail.com>
2023-03-29 22:05:01 -0500

    commit

---
+++ week10/10exc2/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {RouterView, RouterLink} from "vue-router"
+
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/song/test">Song</RouterLink>
+    </nav>
+  <RouterView />
+</template>
+
+<style>
+
+</style>
