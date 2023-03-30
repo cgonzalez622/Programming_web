39efe40b5f7f9e17d66d43e6d26d86276e6f87fb
Camille <c.gonzalez0017@gmail.com>
2023-03-29 22:37:47 -0500

    commit 10ex2

---
+++ week10/10ex2/src/views/Home.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import MusicList from "../data/music-list.json";
+console.log("MusicList", MusicList);
+</script>
+<template>
+    <div class="song-container">
+        <div class="song-list-item" v-for="(song, index) in MusicList" :key="index">
+        <RouterLink :to="'/song/' + song.slug">{{song.title}}</RouterLink>
+        </div>
+    </div>
+</template>
+<style>
+.song-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.song-list-item {
+    width: auto;
+    padding: 20px;
+    margin: 20px;
+    border: 3px solid black;
+    border-radius: 4px;
+    background: white;
+}
+.song-list-item:hover {
+    background: pink;
+}
+.song-list-item a {
+    color: black;
+    text-decoration: none;
+
+}
+.song-list-item:hover a {
+    color: aqua;
+}
+
+</style>
---
+++ week10/10ex2/src/views/Song.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useRoute } from "vue-router";
+import MusicList from "../data/music-list.json"
+const route = useRoute();
+console.log(route.params.slug);
+const song = MusicList.find(song => {
+    return song.slug ===route.params.slug;
+});
+</script>
+<template>
+<div class="song-detail" :class="{
+    rising: song.rank > song.position.positionLastWeek,
+    falling: song.rank < song.position.positionLastWeek,
+    same: song.rank === song.position.positionLastWeek
+    }">
+    <h1>{{song.rank}}</h1>
+    <h2>{{song.title}}</h2>
+    <img :src="song.cover" :alt="song.title">
+    <p>Artist: {{song.artist}}</p>
+    <p></p>
+</div>
+</template>
+<style scoped>
+.song-detail.rising {
+    text-align: center;
+    border: 3px solid blue;
+    margin: 100px;
+}
+.song-detail.falling {
+    text-align: center;
+    border: 3px solid red;
+    margin: 100px;
+}
+.song-detail.same {
+    text-align: center;
+    border: 3px solid yellow;
+    margin: 100px;
+}
+</style>
