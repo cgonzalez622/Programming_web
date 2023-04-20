a1d63bbb732c6697f6ae70879af90f6bc7d115bc
Camille <c.gonzalez0017@gmail.com>
2023-04-19 20:34:00 -0500

    commit 13.2

---
+++ week13/13_2/main.js
@@ -0,0 +1,47 @@
+let horizontal = 50; // distance between each circle
+let horizontal2 = 20; // distance between each circle
+let horizontal3 = 10; // distance between each circle
+let waveWidth;
+let angle = 0.0;
+let amplitude = 100.0;
+let pixelRepeats = 1500.0;
+let dx; // Value for incrementing x
+let yvalues; // Using an array to store height values for the wave
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  waveWidth = width + 1500;
+  dx = (TWO_PI / pixelRepeats) * horizontal;
+  yvalues = new Array(floor(waveWidth / horizontal));
+}
+
+function draw() {
+  background("blue");
+  calcWave();
+  renderWave();
+}
+
+function calcWave() {
+  // speed
+  angle += 0.06;
+
+  // For every x value, calculate a y value with sine function
+  let x = angle;
+  for (let i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x) * amplitude;
+    x += dx;
+  }
+}
+
+function renderWave() {
+  noStroke();
+  fill(0);
+  // A simple way to draw the wave with an ellipse at each location
+  for (let x = 0; x < yvalues.length; x++) {
+
+    ellipse(x * horizontal2, height / 2 + yvalues[x], 16, 16);
+    ellipse(x * horizontal3, height / 2 + yvalues[x], 16, 16);
+
+    
+  }
+}
---
+++ week13/13_2/practice/main.js
@@ -0,0 +1,37 @@
+let slider;
+let slider2;
+let pos;
+
+function setup() {
+    createCanvas(500, 500);
+    angleMode(DEGREES);
+    slider = createSlider(0, 15, 0);
+    slider2 = createSlider(0, 15, 0);
+
+    pos = 0.0;
+    inc = 3.0;
+
+    pos2 = 0.0;
+    inc2 = 9.0;
+}
+
+function draw() {
+
+    clear();
+
+    translate(width/2, height/2);
+
+    let mySinVal = sin(pos);
+    let mySinVal2 = sin(pos2);
+
+    amplified = mySinVal * 50;
+    amplified2 = mySinVal2 * 50;
+
+    for (x = 0; x <= 360; x = x + 5) {
+        curve(0, 10, 50, amplified2, amplified, 10, amplified, 10);
+        rotate(x);
+    }
+    pos = pos + slider.value();
+    pos2 = pos2 + slider.value();
+
+}
