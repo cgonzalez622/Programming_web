7c84991f4e9c450db758216cf728339a5251d6b6
Camille <c.gonzalez0017@gmail.com>
2023-05-07 09:22:48 -0500

    commit

--- week14/final copy/main.js
+++ week14/final copy/main.js
@@ -10,15 +10,16 @@ let notes = [
     {note: "E", freq: 329.6276},
     {note: "C", freq: 261.6256},
     {note: "G", freq: 391.9954}
-]
+];
 
+let starSize = 30;
+let starRotation = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     audioContext = getAudioContext();
     mic = new p5.AudioIn();
     mic.start(listening);
-    fft = new p5.FFT();
 }
 
 //this function is used to see if the code is listening to the input sound (and is connected with the ml5 pitch detection)
@@ -44,13 +45,11 @@ function gotPitch(error, frequency) {
 }
 
 function modelLoaded() {
-
     pitch.getPitch(gotPitch);
 }
 
 function draw(){
-    background(0); //move backgrounf into setup there would be nothing removed already drawn 
-    //but lets make this polished and turned into a website form with a artist statement that covers the what why and how, along with a masthead / and probably credits
+    background(0);
     textAlign(CENTER,CENTER);
     fill(255);
     textSize(32);
@@ -71,6 +70,33 @@ function draw(){
     textSize(64);
     text(closestNote.note, width/2, height - 50);
 
+    //allows for the visual tuning of the array of notes
+    let diff = recordDiff;
+    let diam = map(abs(diff), 0, 100, 255, 0);
+    starSize = map(abs(diff), 0, 100, 100, 150);
 
+    push();
+    translate(width/2, height/2);
+    stroke(255, diam);
+    fill(255, diam);
+    if (abs(diff) < threshold) {
+        fill(0, 255, 0);
+    }
+    star(0, 0, 10, starSize, 20);
+    pop();
+}
 
+function star(x, y, numPoints, outerRadius, innerRadius) {
+    let angle = TWO_PI / numPoints;
+    let halfAngle = angle/2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+        let sx = x + cos(a) * outerRadius;
+        let sy = y + sin(a) * outerRadius;
+        vertex(sx, sy);
+        sx = x + cos(a+halfAngle) * innerRadius;
+        sy = y + sin(a+halfAngle) * innerRadius;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
 }
